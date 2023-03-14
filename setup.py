fca98427158f7b2b500b9f08f1143e1d464fbc7d
PL97 <pengle@126.com>
2023-03-13 21:47:01 -0500

    rename package

--- setup.py
+++ setup.py
@@ -7,15 +7,14 @@ print(this_directory)
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='dcm-parser',
+    name='dcm_parser',
     version='0.1.0',    
     description='A lightning DICOM Parser',
     url='https://github.com/PL97/DICOM_Parser',
     author='Le Peng',
     author_email='peng0347@umn.edu',
     license='MIT',
-    packages=['dcm-parser'],
-    install_requires=['numpy'],
+    packages=['dcm_parser'],
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
